68f7c60cee25e8a5763d90fa7d46bde2f5d42beb
Punit1566 <125143948+Punit1566@users.noreply.github.com>
2025-01-02 05:41:00 +0000

    commit

---
+++ Basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS codes as newer version
+
+// alert(3+3) // abhi node mein use karne ka different tareeka
+// we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Punit")
+
+
+let name = "Punit"//string
+let age=18 //integer
+let isLoggedIn=false //boolean
+
+// number => 2^53
+// bigint => jab number bara hai jaise trading website,stock website
+// string => ""
+// boolean => true/false
+// null => ye datatype hai but ye ek standalone value bhi hai(ek representation hai empty value ka)
+// undefined => value assign nhi hai
+// symbol => jab hm bahut saare componenents bnate hai and hme ek individual component find krna hai then we use symbols
+             //for uniqueness
+// object => null ek object hai but undefined is a type
+
+
+console.log(typeof "Punit")// string
+console.log(typeof undefined)// undefined
+console.log(typeof null)// object
+console.log(typeof age)// number
